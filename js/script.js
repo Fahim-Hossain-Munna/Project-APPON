7ab5740320dee257a5ce21869a6c81c1b704c110
Fahim Hossain Munna <suger.puffs12@gmail.com>
2022-05-24 12:13:56 +0600

    ADD Project

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(".banner-slide").slick({
+
+    arrows: false,
+    dots: true,
+})
+$(".slider-img").slick({
+    slidesToShow: 4,
+    dots: true,
+    arrows: false,
+})
+$('.venobox').venobox();
+
+// $(".main").slick({
+//     dots:false,
+//     arrows:false,
+// })
